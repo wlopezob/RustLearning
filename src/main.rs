abf2c2b560386233a3a336306181ad7139c5e9d0
william luis lopez obregon <wlopezob@gmail.com>
2023-01-17 21:09:54 +0000

    nuevo

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use std::fs;
 fn main() {
 let contenido = fs::read_to_string("src/bucles.rs").unwrap();
 
-  println!("El texto es: {}", contenido);
+  println!("El texto es: {}!", contenido);
   
 }
---
+++ target/debug/deps/my_project-f9bc244fc11a23a8.d
@@ -0,0 +1,5 @@
+/workspaces/RustLearning/target/debug/deps/my_project-f9bc244fc11a23a8.rmeta: src/main.rs
+
+/workspaces/RustLearning/target/debug/deps/my_project-f9bc244fc11a23a8.d: src/main.rs
+
+src/main.rs:
