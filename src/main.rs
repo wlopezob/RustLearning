13de4b32a9949e3e9296d2b3696e0e87ceaaa4e3
william luis lopez obregon <wlopezob@gmail.com>
2023-01-19 01:23:02 +0000

    macro

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,75 @@
-use std::env;
+macro_rules! say_hello {
+    // `()` indicates that the macro takes no argument.
+    () => {
+        // The macro will expand into the contents of this block.
+        println!("Hello!");
+    };
+}
+
+macro_rules! create_function {
+    ($func_name: ident) => {
+        fn $func_name() {
+            println!("You called {:?}()", stringify!($func_name));
+        }
+    };
+}
+
+create_function!(foo);
+create_function!(bar);
+
+macro_rules! print_result {
+    ($expression: expr) => {
+        println!("{:?} = {:?}", stringify!($expression), $expression);
+    };
+}
+
+// `test!` will compare `$left` and `$right`
+// in different ways depending on how you invoke it:
+macro_rules! test {
+    // Arguments don't need to be separated by a comma.
+    // Any template can be used!
+    ($left:expr; and $right:expr) => {
+        println!("{:?} and {:?} is {:?}",
+                 stringify!($left),
+                 stringify!($right),
+                 $left && $right)
+    };
+    // ^ each arm must end with a semicolon.
+    ($left:expr; or $right:expr) => {
+        println!("{:?} or {:?} is {:?}",
+                 stringify!($left),
+                 stringify!($right),
+                 $left || $right)
+    };
+}
+
+// `find_min!` will calculate the minimum of any number of arguments.
+macro_rules! find_min {
+    // Base case:
+    ($x:expr) => ($x);
+    // `$x` followed by at least one `$y,`
+    ($x:expr, $($y:expr),+) => (
+        // Call `find_min!` on the tail `$y`
+        std::cmp::min($x, find_min!($($y),+))
+    )
+}
 
 fn main() {
-    let argumentos: Vec<String> = env::args().collect();
-    dbg!(&argumentos);
+    say_hello!();
 
-    if (argumentos.len() > 2) {
-        let parametro1 = &argumentos[1];
-        let parametro2 = &argumentos[1];
+    foo();
+    bar();
 
-        println!("El primer parametro es: {}", parametro1);
-        println!("El segundo parametro es: {}", parametro2);
-    } else {
-        println!("El formato es incorrecto");
-    }
+    print_result!(1u32 + 1);
+    print_result!({
+        let x = 1i32;
+        x * x + 2 *x -1
+    });
+
+    test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
+    test!(true; or false);
+
+    println!("{}", find_min!(1));
+    println!("{}", find_min!(1 + 2, 2));
+    println!("{}", find_min!(5, 2 * 3, 4,-2));
 }
